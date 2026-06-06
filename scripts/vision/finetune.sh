0482e2a9b46ddd402bd7cd47d4c0c393c5bf5c43
Marawan Gamal <marawan.gamal@live.com>
2026-06-06 19:25:04 -0400

    Chore: update vision rerun setup

--- scripts/vision/finetune.sh
+++ scripts/vision/finetune.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=finetune_vision
 #SBATCH --partition=long
-#SBATCH --gres=gpu:l40s:1
+#SBATCH --gres=gpu:rtx8000:1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=48G
 #SBATCH --time=12:00:00
